7981f95275ea24ef8915aede3a6146a3891d0aba
vaibhav <vaibhavsharma38@gmail.com>
2016-02-22 13:19:58 +0530

    grid added in opportunity and lead

--- lib/Model/Lead.php
+++ lib/Model/Lead.php
@@ -6,7 +6,7 @@ class Model_Lead extends \xepan\base\Model_Contact{
 
 	function init(){
 		parent::init();
-
+		
 		$lead_j=$this->join('lead.contact_id');
 		$lead_j->addField('source');
 		$lead_j->addField('communication');
--- lib/Model/Opportunity.php
+++ lib/Model/Opportunity.php
@@ -2,14 +2,14 @@
 
 namespace xepan\marketing;  
 
-class Model_Opportunity extends \xepan\base\Model_document{
+class Model_Opportunity extends \xepan\base\Model_Document{
 
 	function init(){
 		parent::init();
 
-		$this->hasOne('xepan\base\contact');
-		$opp_j=$this->join('Opportunity.document_id');
-		$opp_j->addField('details');
+		$opp_j=$this->join('opportunity.document_id');
+		$opp_j->hasOne('xepan\marketing\Lead','contact_id');
+		$opp_j->addField('title');
 		$opp_j->addField('duration');
 
 	}

46684cc76d3e18eb65c4d5b77454bff0bcd373b4
vaibhav <vaibhavsharma38@gmail.com>
2016-02-26 12:47:14 +0530

    category filter added on lead page

--- page/lead.php
+++ page/lead.php
@@ -7,7 +7,14 @@ class page_lead extends \Page{
 	function init(){
 		parent::init();
 
+		$category_id = $this->api->stickyGET('category_id');
+
 		$lead = $this->add('xepan\marketing\Model_Lead');
+
+		if(is_numeric($category_id)){
+			$lead->addCondition('category_id',$category_id);
+		}
+
 		$crud = $this->add('xepan\hr\CRUD',
 						[
 							'action_page'=>'xepan_marketing_leaddetails',
@@ -18,5 +25,39 @@ class page_lead extends \Page{
 		$crud->setModel($lead);
 		$crud->grid->addQuickSearch(['name']);
 
+		$g = $crud->grid;
+		//Filter Form category wise
+		$form = $g->add('Form',null,'filter',['form/empty'])->addClass('pull-right');
+		//Add Fields
+		$category_field = $form->addField('DropDown','category');
+		$category_field->setModel('xepan/marketing/Model_LeadCategory');
+		$category_field->setEmptyText('Select Category');
+		$category_field->set($category_id);
+		//adding custom submit button
+		$form_btn = $category_field->add('Button',null,'after_field')->set(['go','icon'=>'x fa fa-search']);
+		
+		
+		//Form Search Btn
+		$form_btn->js('click',$form->js()->submit());
+
+		if(is_numeric($category_id)){
+			$form_reset_btn = $category_field->add('Button',null,'before_field')->set(['','icon'=>'x fa fa-times ']);
+			//Form Reset Btn
+			$self = $this;
+			$form_reset_btn->on('click',function($f)use($self){
+				$self->api->stickyForget('category_id');
+				return $self->js()->univ()->location($self->api->url(null));
+			});
+		}
+
+		//form submission handle
+		$form->onSubmit(function($form){
+				if(!$form['category'])
+				return $form->error('category','please select');
+
+			return $form->js()->univ()->location($form->api->url(null,['category_id'=>$form['category']]));
+		});
+
+		
 	}
 }

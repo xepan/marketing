533c7e05c8135f75cb1a90037528903c13f1a452
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-02 09:42:26 +0530

    Menu position changed

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -13,12 +13,15 @@ class Initiator extends \Controller_Addon {
 
 		if($this->app->is_admin){
 			$m = $this->app->top_menu->addMenu('Marketing');
+			$m->addItem('Category Management','xepan_marketing_marketingcategory');
 			$m->addItem('Lead','xepan_marketing_lead');
 			$m->addItem('Opportunity','xepan_marketing_opportunity');
 			$m->addItem('Newsletter','xepan_marketing_newsletter');
 			$m->addItem('Social','xepan_marketing_socialcontent');
 			$m->addItem('SMS','xepan_marketing_sms');
-			$m->addItem('Category Management','xepan_marketing_marketingcategory');
+			$m->addItem('Schedular','xepan_marketing_schedule');
+			$m->addItem('Report','xepan_marketing_report');
+
 		}
 		
 	}

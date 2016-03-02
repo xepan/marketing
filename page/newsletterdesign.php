a91ea2d338065d729f9e7ecfc0d6c1adb7bc0c79
vaibhav <vaibhavsharma38@gmail.com>
2016-03-02 14:27:29 +0530

    newsletter design page made.

---
+++ page/newsletterdesign.php
@@ -0,0 +1,15 @@
+<?php
+namespace xepan\marketing;
+class page_newsletterdesign extends \Page{
+	public $title="Newsletter Design";
+	function init(){
+		parent::init();	
+
+		$action = $this->api->stickyGET('action')?:'view';
+		$newsletter = $this->add('xepan\marketing\Model_Newsletter');
+		//$newsletter->addCondition('is_template',true);
+		//$crud=$this->add('xepan\base\Grid',null,null,['grid/newslettertemplate-grid']);
+		$crud = $this->add('xepan\hr\View_Document',['action'=> $action],null,['view/newsletterdesign']);
+		$crud->setModel($newsletter);
+	}
+}
--- page/newslettertemplate.php
+++ page/newslettertemplate.php
@@ -5,9 +5,11 @@ class page_newslettertemplate extends \Page{
 	function init(){
 		parent::init();	
 
+		//$action = $this->api->stickyGET('action')?:'view';
 		$newsletter = $this->add('xepan\marketing\Model_Newsletter');
 		$newsletter->addCondition('is_template',true);
-		$crud=$this->add('xepan\base\Grid',null/*['action_page'=>'xepan_marketing_newsletterdesign']*/,null,['grid/newslettertemplate-grid']);
+		$crud=$this->add('xepan\base\Grid',null,null,['grid/newslettertemplate-grid']);
+		//$crud = $this->add('xepan\hr\View_Document',['action'=> $action],null,['view/newsletterdesign']);
 		$crud->setModel($newsletter);
 	}
 }

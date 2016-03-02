460e50db01b4db12a801e0b34fb5ed17d7b26c47
vaibhav <vaibhavsharma38@gmail.com>
2016-03-02 15:42:57 +0530

    newsletter  create new design view create

--- page/newsletterdesign.php
+++ page/newsletterdesign.php
@@ -5,11 +5,11 @@ class page_newsletterdesign extends \Page{
 	function init(){
 		parent::init();	
 
-		$action = $this->api->stickyGET('action')?:'view';
+		// $action = $this->api->stickyGET('action')?:'view';
 		$newsletter = $this->add('xepan\marketing\Model_Newsletter');
 		//$newsletter->addCondition('is_template',true);
 		//$crud=$this->add('xepan\base\Grid',null,null,['grid/newslettertemplate-grid']);
-		$crud = $this->add('xepan\hr\View_Document',['action'=> $action],null,['view/newsletterdesign']);
+		$crud = $this->add('xepan\hr\View_Document',null,null,['view/newsletterdesign']);
 		$crud->setModel($newsletter);
 	}
 }

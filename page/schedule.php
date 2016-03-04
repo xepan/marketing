cfacfe4b2feb85f094a2e8a8b450a506b9fa3cb2
vaibhav <vaibhavsharma38@gmail.com>
2016-03-04 09:44:42 +0530

    modified the grids(edit delete button)

--- page/schedule.php
+++ page/schedule.php
@@ -9,6 +9,6 @@ class page_schedule extends \Page{
 		$schedule = $this->add('xepan\marketing\Model_Sms')->tryLoadBy('id',$this->api->stickyGET('document_id'));
 
 		$sch = $this->add('xepan\hr\View_Document',['action'=>$action],null,['view/schedule']);
-	    $sch->setModel($schedule);
+	    $sch->setModel($schedule,['x'],['y']);
 	}
 }

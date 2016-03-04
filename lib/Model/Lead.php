e81fd664fc868521c603e00a1f7959dbc2e8e0aa
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-04 09:49:29 +0530

    Added avatar and some expressions like opportunity counts in lead

--- lib/Model/Lead.php
+++ lib/Model/Lead.php
@@ -23,6 +23,27 @@ class Model_Lead extends \xepan\base\Model_Contact{
 		$lead_j->hasOne('xepan\marketing\MarketingCategory','marketing_category_id');
 		$lead_j->addField('source');
 		$lead_j->hasMany('xepan\marketing\Opportunity','lead_id',null,'Opportunity');
+
+		$this->addExpression('open_count')->set(function($m,$q){
+			return $this->add('xepan\marketing\Model_Opportunity',['table_alias'=>'open_count'])
+						->addCondition('lead_id',$q->getField('id'))
+						->addCondition('status','Open')
+						->count();
+		});
+
+		$this->addExpression('converted_count')->set(function($m,$q){
+			return $this->add('xepan\marketing\Model_Opportunity',['table_alias'=>'converted_count'])
+						->addCondition('lead_id',$q->getField('id'))
+						->addCondition('status','Converted')
+						->count();
+		});
+
+		$this->addExpression('rejected_count')->set(function($m,$q){
+			return $this->add('xepan\marketing\Model_Opportunity',['table_alias'=>'rejected_count'])
+						->addCondition('lead_id',$q->getField('id'))
+						->addCondition('status','Rejected')
+						->count();
+		});
 		
 		$this->addCondition('type','Lead');
 

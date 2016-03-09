b2bbe356525b24bfc6a41ac462a015bda98d91d4
vijay mali <guddavijay@gmail.com>
2016-03-09 14:02:24 +0530

    add Hook campaign category before delete first child campaign

--- lib/Model/CampaignCategory.php
+++ lib/Model/CampaignCategory.php
@@ -25,5 +25,16 @@ class Model_CampaignCategory extends \xepan\base\Model_Document{
 		$cat_j->hasMany('xepan\marketing\Campaign','campaign_category_id');
 		//$this->addCondition('type','CampaignCategory');
 
+		$this->addHook('beforeSave',$this);
+		$this->addHook('beforeDelete',$this);
+	}
+
+	function beforeSave($m){}
+
+	function beforeDelete($m){
+		$campaign_count = $m->ref('xepan\marketing\Campaign')->count()->getOne();
+		
+		if($campaign_count)
+			throw $this->exception('Cannot Delete,first delete Campaign`s ');	
 	}
 }

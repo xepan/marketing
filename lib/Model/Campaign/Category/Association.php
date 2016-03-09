446ed3797fcc75b09a46016c30acce518ded39bb
vijay mali <guddavijay@gmail.com>
2016-03-09 14:47:23 +0530

    Campaign Assocation model move to folder structure

--- lib/Model/Campaign/Category/Association.php
+++ lib/Model/Campaign/Category/Association.php
@@ -2,7 +2,7 @@
 
 namespace xepan\marketing;
 
-class Model_CampaignCategory extends \xepan\base\Model_Document{
+class Model_Campaign_Category_Association extends \xepan\base\Model_Document{
 
 	public $status=[
 
@@ -22,8 +22,8 @@ class Model_CampaignCategory extends \xepan\base\Model_Document{
 		$cat_j = $this->join('campaigncategory.document_id');
 		$cat_j->addField('name');
 		
-		$cat_j->hasMany('xepan\marketing\MarketingCategory','campaign_category_id');
-		$cat_j->hasMany('xepan\marketing\Campaign','campaign_category_id');
+		$cat_j->hasOne('xepan\marketing\MarketingCategory','marketing_category_id');
+		$cat_j->hasOne('xepan\marketing\Campaign','Camapign_id');
 		//$this->addCondition('type','CampaignCategory');
 
 		$this->addHook('beforeSave',$this);
@@ -34,6 +34,7 @@ class Model_CampaignCategory extends \xepan\base\Model_Document{
 
 	function beforeDelete($m){
 		$campaign_count = $m->ref('xepan\marketing\Campaign')->count()->getOne();
+		$marketing_count = $m->ref('xepan\marketing\MarketingCategory')->count()->getOne();
 		
 		if($campaign_count)
 			throw $this->exception('Cannot Delete,first delete Campaign`s ');	

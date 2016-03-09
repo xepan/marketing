0b5a052331516bb057581e4771554fc84d8a6243
vijay mali <guddavijay@gmail.com>
2016-03-09 14:07:25 +0530

    add Hook Lead & Marketing Category before delete first child

--- lib/Model/MarketingCategory.php
+++ lib/Model/MarketingCategory.php
@@ -23,9 +23,22 @@ class Model_MarketingCategory extends \xepan\base\Model_Document{
 		$cat_j->addField('name');
 		
 		$cat_j->hasMany('xepan\marketing\Lead','marketing_category_id');
+		$cat_j->hasMany('xepan\marketing\Content','marketing_category_id');
 		$this->addExpression('leads_count')->set($this->refSql('xepan\marketing\Lead')->count());
 		$this->addCondition('type','MarketingCategory');
-		//$this->debug();
 
+		$this->addHook('beforeSave',$this);
+		$this->addHook('beforeDelete',$this);
+
+	}
+
+	function beforeSave($m){}
+
+	function beforeDelete($m){
+		$lead_count = $m->ref('xepan\marketing\Lead')->count()->getOne();
+		$content_count = $m->ref('xepan\marketing\Content')->count()->getOne();
+		
+		if($lead_count or $content_count)
+			throw $this->exception('Cannot Delete,first delete Lead`s And Contents ');	
 	}
 }

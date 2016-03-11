af1738db998e40aa01a0479080da368663beaaf9
vaibhav <vaibhavsharma38@gmail.com>
2016-03-11 22:33:13 +0530

    slight changes in templates.

--- page/addsms.php
+++ page/addsms.php
@@ -8,7 +8,7 @@ class page_addsms extends \Page{
 		$action = $this->api->stickyGET('action')?:'view';
 		$sms = $this->add('xepan\marketing\Model_Sms')->tryLoadBy('id',$this->api->stickyGET('document_id'));
 
-		$sv = $this->add('xepan\hr\View_Document',['action'=>$action],null,['view/addsms']);
+		$sv = $this->add('xepan\hr\View_Document',['action'=>$action,'id_field_on_reload'=>'content_id'],null,['view/addsms']);
 	    $sv->setModel($sms,['title','message_160','marketing_category_id'],['title','message_160','marketing_category_id']);
 	}
 }
--- page/addsocialpost.php
+++ page/addsocialpost.php
@@ -8,7 +8,8 @@ class page_addsocialpost extends \Page{
 		$action = $this->api->stickyGET('action')?:'view';
 		$social = $this->add('xepan\marketing\Model_SocialPost')->tryLoadBy('id',$this->api->stickyGET('document_id'));
 
-		$sv = $this->add('xepan\hr\View_Document',['action'=>$action],null,['view/addsocialpost']);
+		$sv = $this->add('xepan\hr\View_Document',['action'=>$action,'id_field_on_reload'=>'content_id'],null,['view/addsocialpost']);
+
 	    $sv->setModel($social,['title','message_160','message_255','message_3000','message_blog','attachment','url','marketing_category_id'],['title','message_160','message_255','message_3000','message_blog','attachment','url','marketing_category_id']);
 	}
 }

b27dad04fddfff543a65938640d7984aefd60d29
vaibhav <vaibhavsharma38@gmail.com>
2016-03-12 14:54:21 +0530

    strange ~_^

--- page/opportunity.php
+++ page/opportunity.php
@@ -2,22 +2,20 @@
 namespace xepan\marketing;
 
 class page_opportunity extends \Page{
-
-	public $title="Opportunity";
+	public $title ="Opportunity";
 
 	function init(){
-		parent::init();
-
-		$opportunity=$this->add('xepan\marketing\Model_Opportunity');
-
-		$crud=$this->add('xepan\hr\CRUD',null,null,['grid/opportunity-grid']);
-
-		$crud->setModel($opportunity);
+		parent::init(); 
+ 
+		$opportunity = $this->add('xepan\marketing\Model_Opportunity');
 		
-		$crud->grid->addQuickSearch(['lead']);
-
+		$crud = $this->add('xepan\hr\CRUD',null,null,['grid/opportunity-grid']);
+		$crud->setModel($opportunity);		
 		$crud->add('xepan\base\Controller_Avatar',['name_field'=>'lead']);
-
+		
+		$f = $crud->grid->addQuickSearch(['lead']);
+		$dropdown = $f->addField('dropdown','status')->setValueList(['Open'=>'Open','Converted'=>'Converted','Rejected'=>'Rejected'])->setEmptyText('Status');
+		$dropdown->js('change',$f->js()->submit());
 	}
 
 }

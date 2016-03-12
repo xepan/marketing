efdd855ba7a992ed2ff2889cab5e74d7a76f0492
vaibhav <vaibhavsharma38@gmail.com>
2016-03-12 11:56:08 +0530

    iframe added in nesletter page(having trouble in 'src' and HTML code is not converted !!) and few other minor changes

--- page/addcampaign.php
+++ page/addcampaign.php
@@ -8,7 +8,7 @@ class page_addcampaign extends \Page{
 		$action = $this->api->stickyGET('action')?:'view';
 		$campaign = $this->add('xepan\marketing\Model_Campaign')->tryLoadBy('id',$this->api->stickyGET('document_id'));
 
-		$camp = $this->add('xepan\hr\View_Document',['action'=>$action],null,['view/addcampaign']);
+		$camp = $this->add('xepan\hr\View_Document',['action'=>$action,'id_field_on_reload'=>'campaign_id'],null,['view/addcampaign']);
 	    $camp->setModel($campaign,['title', 'starting_date', 'ending_date', 'campaign_category', 'campaign_type'],['title', 'starting_date', 'ending_date', 'campaign_category_id', 'campaign_type']);
 	}
 }
--- page/newsletterdesign.php
+++ page/newsletterdesign.php
@@ -8,8 +8,7 @@ class page_newsletterdesign extends \Page{
 		$action = $this->api->stickyGET('action')?:'view';
 		$newsletter = $this->add('xepan\marketing\Model_Newsletter')->tryLoadBy('id',$this->api->stickyGET('document_id'));
 
-		$nv = $this->add('xepan\hr\View_Document',['action'=>$action],null,['view/newsletterdesign']);
-		$nv->setIdField('document_id');
-		$nv->setModel($newsletter,['title','message_3000','marketing_category','created_by','created_at'],['title','message_3000','marketing_category_id']);
+		$nv = $this->add('xepan\hr\View_Document',['action'=>$action,'id_field_on_reload'=>'content_id'],null,['view/newsletterdesign']);
+		$nv->setModel($newsletter,['title','message_blog','marketing_category','created_by','created_at'],['title','message_blog','marketing_category_id']);
 	}
 }

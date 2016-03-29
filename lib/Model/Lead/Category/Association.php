d785dcd7f636d919567e3f87fa1ab68bd70ef120
vaibhav <vaibhavsharma38@gmail.com>
2016-03-29 15:41:46 +0530

    lead associated with many categories

---
+++ lib/Model/Lead/Category/Association.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace xepan\marketing;
+
+class Model_Lead_Category_Association extends \xepan\base\Model_Table{
+	public $table = "lead_category_association";
+
+	public $status=[
+
+	];
+	public $actions=[
+		'*'=>[
+			'add',
+			'view',
+			'edit',
+			'delete'
+		]
+	];
+	public $acl=false;
+
+	function init(){
+		parent::init();
+		
+		
+		$this->hasOne('xepan\marketing\Lead','lead_id');
+		$this->hasOne('xepan\marketing\MarketingCategory','marketing_category_id');
+
+	}	
+}

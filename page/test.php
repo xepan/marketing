c68592a9251d306cc754356209bb64b0482f0d41
vaibhav <vaibhavsharma38@gmail.com>
2016-04-02 13:06:42 +0530

    newsletter send, without throttle.

--- page/test.php
+++ page/test.php
@@ -29,9 +29,11 @@ class page_test extends \Page {
 		
 		$schedule_j = $camp_j->join('schedule.campaign_id','document_id');
 		$schedule_j->hasOne('xepan/marketing/Content','document_id','title');
+
 		$schedule_j->addField('schedule_date','date');
 		$schedule_j->addField('schedule_day','day');
 
+		
 		$comm_j = $schedule_j->leftJoin('communication.related_id','document_id');
 		$schedule_j->addField('date');
 
@@ -65,11 +67,45 @@ class page_test extends \Page {
 				]);
 		});
 
+		$lead->addExpression('body')->set(function($m,$q){
+			return $m->refSQL('document_id')->fieldQuery('message_3000');
+		});
+
 		$lead->addCondition('last_sent_newsletter_from_schedule_row_days','>=',10);
 		$lead->addCondition('sendable',true);
 
-		$grid= $this->add('Grid');
-		$grid->setModel($lead->debug(),['name','document','campaign_title','campaign_type','schedule_day','days_from_join','sendable','last_sent_newsletter_from_schedule_row_days']);
+
+
+		// $grid= $this->add('Grid');
+		// $grid->setModel($lead->debug(),['name','document','campaign_title','campaign_type','schedule_day','days_from_join','sendable','last_sent_newsletter_from_schedule_row_days']);
+
+
+		$model_communication_newsletter = $this->add('xepan\marketing\Model_Communication_Newsletter');
+		$email_settings = $this->add('xepan\base\Model_Epan_EmailSetting')->tryLoadAny();
+		$model_communication_newsletter->setfrom($email_settings['from_email'],$email_settings['from_name']);
+
+		$form = $this->add('Form');
+		$form->addSubmit('Send Newsletter');
+
+		if($form->isSubmitted()){
+			foreach ($lead as $newsletter) {
+				
+				$emails = $lead->getEmails();
+			    $subject = $lead['document'] ;		    		    
+				$body = $lead['body'];
+
+				foreach ($emails as $email) {	
+					$model_communication_newsletter->addTo($email);
+				}
+
+				$model_communication_newsletter->setSubject($subject);
+				$model_communication_newsletter->setBody($body);
+				// $model_communication_newsletter->send();
+				$model_communication_newsletter->save();
+			}
+
+			return $form->js()->univ()->successMessage('Newsletter Send')->execute();
+		}
 
 	}
 }

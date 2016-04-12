bce95b3dc1e7735786d423b17e7e54d3c2cb9b44
Nillam Joshi <nilam.joshi49@gmail.com>
2016-04-12 16:25:31 +0530

    add icons in marketing menu..

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -14,15 +14,14 @@ class Initiator extends \Controller_Addon {
 
 		if($this->app->is_admin){
 			$m = $this->app->top_menu->addMenu('Marketing');
-			$m->addItem('Category Management','xepan_marketing_marketingcategory');
-			$m->addItem('Lead','xepan_marketing_lead');
-			$m->addItem('Opportunity','xepan_marketing_opportunity');
-			$m->addItem('Newsletter','xepan_marketing_newsletter');
-			$m->addItem('Social','xepan_marketing_socialcontent');
-			$m->addItem('SMS','xepan_marketing_sms');
-			$m->addItem('Campaign','xepan_marketing_campaign');
-			$m->addItem('Report','xepan_marketing_report');
-
+			$m->addItem(['Category Management','icon'=>'fa fa-table'],'xepan_marketing_marketingcategory');
+			$m->addItem(['Lead','icon'=>'fa fa-desktop'],'xepan_marketing_lead');
+			$m->addItem(['Opportunity','icon'=>'fa fa-archive'],'xepan_marketing_opportunity');
+			$m->addItem(['Newsletter','icon'=>'fa fa-copy'],'xepan_marketing_newsletter');
+			$m->addItem(['Social','icon'=>'fa fa-cubes'],'xepan_marketing_socialcontent');
+			$m->addItem(['SMS','icon'=>'fa fa-envelope'],'xepan_marketing_sms');
+			$m->addItem(['Campaign','icon'=>'fa fa-sliders'],'xepan_marketing_campaign');
+			$m->addItem(['Report','icon'=>'fa fa-file-text-o'],'xepan_marketing_report');
 		}
 		
 	}

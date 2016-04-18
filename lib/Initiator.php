db29fac93ed96c4c8504e233855ab4f67913b4c0
vijay mali <guddavijay@gmail.com>
2016-04-18 15:04:19 +0530

    check isset $this->app->epan

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -28,6 +28,9 @@ class Initiator extends \Controller_Addon {
 
 	function generateInstaller(){
 		// Clear DB
+		if(!isset($this->app->old_epan)) $this->app->old_epan = $this->app->epan;
+        if(!isset($this->app->new_epan)) $this->app->new_epan = $this->app->epan;
+
         $this->app->epan=$this->app->old_epan;
         $truncate_models = ['Opportunity','Lead_Category_Association','Lead','Campaign_Category_Association','Schedule','Campaign_SocialUser_Association','SocialUser','campaign','Content','MarketingCategory'];
         foreach ($truncate_models as $t) {

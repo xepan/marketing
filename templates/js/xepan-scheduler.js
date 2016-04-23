de0c18958752f118487155328b33ee7b66b1d36e
vaibhav <vaibhavsharma38@gmail.com>
2016-04-23 11:04:32 +0530

    on click prompt from calendar cell removed

--- templates/js/xepan-scheduler.js
+++ templates/js/xepan-scheduler.js
@@ -34,18 +34,18 @@ $.each({
 			selectable: true,
 			selectHelper: true,
 			select: function(start, end, allDay) {
-				var title = prompt('Event Title:');
-				if (title) {
-					calendar.fullCalendar('renderEvent',
-						{
-							title: title,
-							start: start,
-							end: end,
-							allDay: allDay
-						},
-						true // make the event "stick"
-					);
-				}
+				// var title = prompt('Event Title:');
+				// if (title) {
+				// 	calendar.fullCalendar('renderEvent',
+				// 		{
+				// 			title: title,
+				// 			start: start,
+				// 			end: end,
+				// 			allDay: allDay
+				// 		},
+				// 		true // make the event "stick"
+				// 	);
+				// }
 				calendar.fullCalendar('unselect');
 			},
 			editable: true,

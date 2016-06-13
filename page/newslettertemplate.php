5c37b6fee718b1e1fc7139e6224b966d4ce26768
vaibhav <vaibhavsharma38@gmail.com>
2016-06-13 18:08:46 +0530

    changes made in newsletter page and social post page

--- page/newslettertemplate.php
+++ page/newslettertemplate.php
@@ -8,7 +8,7 @@ class page_newslettertemplate extends \xepan\base\Page{
 		parent::init();	
 
 		$newsletter = $this->add('xepan\marketing\Model_Newsletter');
-		// $newsletter->addCondition('is_template',true);
+		$newsletter->addCondition('is_template',true);
 		$newsletter->tryLoadAny();		
 		$crud=$this->add('xepan\base\Grid',null,null,['grid/newslettertemplate-grid']);
 		$crud->setModel($newsletter);

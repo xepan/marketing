0069478b7367468c246103c168a22b9880f7eab9
vaibhav <vaibhavsharma38@gmail.com>
2016-06-25 14:47:34 +0530

    template editing feature added

--- page/newslettertemplate.php
+++ page/newslettertemplate.php
@@ -2,15 +2,14 @@
 namespace xepan\marketing;
 class page_newslettertemplate extends \xepan\base\Page{
 	public $title="Newsletter Template";
-	public $breadcrumb=['Home'=>'index','Newsletter'=>'xepan_marketing_newslettertemplate','Template'=>'#'];
+	public $breadcrumb=['Home'=>'index','Newsletter'=>'xepan_marketing_newsletter','Template'=>'#'];
 
 	function init(){
 		parent::init();	
 
 		$newsletter = $this->add('xepan\marketing\Model_Newsletter');
 		$newsletter->addCondition('is_template',true);
-		$newsletter->tryLoadAny();		
-		$crud=$this->add('xepan\base\Grid',null,null,['grid/newslettertemplate-grid']);
-		$crud->setModel($newsletter);
+		$crud=$this->add('xepan\hr\CRUD',null,null,['grid/newslettertemplate-grid']);
+		$crud->setModel($newsletter,['title','message_blog','marketing_category_id']);
 	}
 }

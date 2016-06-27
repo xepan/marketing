b33f09cdace1998e676ef74a5e4c43cebcf64af9
vijay mali <guddavijay@gmail.com>
2016-06-27 12:51:38 +0530

    lead total visitor count display on grid & click to open total visitor details

---
+++ page/leadvisitor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace xepan\marketing;
+	
+class page_leadvisitor extends \xepan\base\Page{
+	public $title = "Total Visitors";
+	function init(){
+		parent::init();
+
+		$lead_id=$this->app->stickyGET('contact_id');
+		
+		$visitors=$this->add('xepan\marketing\Model_LandingResponse');
+		$visitors->addCondition('contact_id',$lead_id);
+		$visitors->tryLoadAny();
+		$crud = $this->add('xepan\hr\CRUD');
+		$crud->setModel($visitors);
+		$crud->grid->addPaginator(50);
+	}
+}

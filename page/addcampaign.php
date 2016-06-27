030dd88f14a4daa9fd5fd1c68f64411d06d9b31c
vaibhav <vaibhavsharma38@gmail.com>
2016-06-27 18:30:59 +0530

    bug in campaign solved

--- page/addcampaign.php
+++ page/addcampaign.php
@@ -10,7 +10,7 @@ class page_addcampaign extends \xepan\base\Page{
 		$action = $this->api->stickyGET('action')?:'view';
 		$campaign = $this->add('xepan\marketing\Model_Campaign')->tryLoadBy('id',$this->api->stickyGET('document_id'));
 
-		$camp = $this->add('xepan\hr\View_Document',['action'=>$action,'id_field_on_reload'=>'campaign_id'],null,['view/addcampaign']);
+		$camp = $this->add('xepan\hr\View_Document',['action'=>$action,'id_field_on_reload'=>'document_id'],null,['view/addcampaign']);
 	    $camp->setModel($campaign,['title', 'starting_date', 'ending_date', 'campaign_type'],['title', 'starting_date', 'ending_date', 'campaign_type']);
 	}
 }

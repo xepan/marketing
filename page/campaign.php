d101db4dddc7b6e590e118848bff7671eaefadf5
vaibhav <vaibhavsharma38@gmail.com>
2016-07-04 14:55:14 +0530

    working on campaign page

--- page/campaign.php
+++ page/campaign.php
@@ -8,12 +8,21 @@ class page_campaign extends \xepan\base\Page{
 		$campaign = $this->add('xepan\marketing\Model_Campaign');
 		if($this->app->stickyGET('status'))
 			$campaign->addCondition('status',explode(",",$this->app->stickyGET('status')));
-				
+		
+		$campaign->addExpression('completed_percentage')->set(function($m, $q){
+			return $m->dsql()->expr("ROUND(([1]-[0])/[1]*100,0)",[$m->getElement('total_postings'),$m->getElement('remaining')]);
+		});
+		
 		$campaign->add('xepan\marketing\Controller_SideBarStatusFilter');
 		$crud=$this->add('xepan\hr\CRUD',['action_page'=>'xepan_marketing_addcampaign'],null,['grid/campaign-grid']);
 		$crud->setModel($campaign);
-
+		$crud->grid->js(true)->_load('jquery.sparkline.min')->_selector('.sparkline')->sparkline('html', ['enableTagOptions' => true]);
 		$frm=$crud->grid->addQuickSearch(['title']);
 														
 	}
+
+	function render(){
+		$this->app->jui->addStaticInclude('jquery.easypiechart.min');
+		parent::render();
+	}	
 }

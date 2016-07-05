54ed278f93cce8ab4caef25f36486afee48dfa66
vaibhav <vaibhavsharma38@gmail.com>
2016-07-05 10:02:15 +0530

    expressions added on campaign page and social grid improved

--- page/campaign.php
+++ page/campaign.php
@@ -8,11 +8,27 @@ class page_campaign extends \xepan\base\Page{
 		$campaign = $this->add('xepan\marketing\Model_Campaign');
 		
 		$campaign->addExpression('source_graph_data')->set(function($m,$q){
-			return "''";
+			$lr = $m->add('xepan\marketing\Model_LandingResponse');
+			$lr->_dsql()->del('fields');
+			$lr->_dsql()->field('count(*) visits');
+			$lr->_dsql()->field('campaign_id');
+			$lr->_dsql()->field('type');
+			$lr->_dsql()->group('campaign_id');
+			$lr->_dsql()->group('type');
+
+			return $q->expr("(select GROUP_CONCAT(concat(tmp.visits,'/',tmp.type)) from [sql] as tmp where tmp.campaign_id = [0])",[$q->getField('id'),'sql'=>$lr->_dsql()]);
 		});
 
 		$campaign->addExpression('timing_graph_data')->set(function($m,$q){
-			return "''";
+			$lr = $m->add('xepan\marketing\Model_LandingResponse');
+			$lr->_dsql()->del('fields');
+			$lr->_dsql()->field('count(*) visits');
+			$lr->_dsql()->field('campaign_id');
+			$lr->_dsql()->field('HOUR(date) timeslot');
+			$lr->_dsql()->group('campaign_id');
+			$lr->_dsql()->group('HOUR(date)');
+
+			return $q->expr("(select GROUP_CONCAT(concat(tmp.visits,'/',tmp.timeslot)) from [sql] as tmp where tmp.campaign_id = [0])",[$q->getField('id'),'sql'=>$lr->_dsql()]);
 		});
 
 		if($this->app->stickyGET('status'))

0ccf33cda893db161dee429d72aa4ae0638163de
vaibhav <vaibhavsharma38@gmail.com>
2016-07-05 16:11:09 +0530

    sparkline added for communication

--- page/leaddetails.php
+++ page/leaddetails.php
@@ -14,9 +14,16 @@ class page_leaddetails extends \xepan\base\Page {
 		$lead= $this->add('xepan\marketing\Model_Lead')->tryLoadBy('id',$this->api->stickyGET('contact_id'));
 		
 		$lead->addExpression('weakly_communication')->set(function($m,$q){
-			return "'1,2,3,4,5,6,7'";	
-		});	
+			$comm = $m->add('xepan/communication/Model_Communication');
+			// $comm->addCondition('sent_on','>',date('Y-m-d',strtotime('-8 week')));
+			$comm->_dsql()->del('fields');
+			$comm->_dsql()->field('count(*) communication_count');
+			$comm->_dsql()->field('to_id');
+			$comm->_dsql()->group('to_id');
 
+			return $q->expr("(select GROUP_CONCAT(tmp.communication_count) from [sql] as tmp where tmp.to_id = [0])",[$m->getElement('id'),'sql'=>$comm->_dsql()]);
+		});
+		
 		if($action=="add"){
 
 			$lead_view = $this->add('xepan\base\View_Contact',['acl'=>'xepan\marketing\Model_Lead','view_document_class'=>'xepan\hr\View_Document'],'contact_view_full_width');

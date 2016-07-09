46e8af5e681485c1f1c7566c2822e340eb077d15
vijay mali <guddavijay@gmail.com>
2016-07-09 15:34:59 +0530

    add expression score & click to open score details

--- page/lead.php
+++ page/lead.php
@@ -45,8 +45,9 @@ class page_lead extends \xepan\base\Page{
 		$crud->grid->addFormatter('category','marketingcategory');
 		$crud->grid->js(true)->_load('jquery.sparkline.min')->_selector('.sparkline')->sparkline('html', ['enableTagOptions' => true]);
 		if(!$crud->isEditing()){
-		$crud->grid->js('click')->_selector('.do-view-lead')->univ()->frameURL('Lead Details',[$this->api->url('xepan_marketing_leaddetails'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
-		$crud->grid->js('click')->_selector('.do-view-lead-visitor')->univ()->frameURL('Total Visitors',[$this->api->url('xepan_marketing_leadvisitor'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
+			$crud->grid->js('click')->_selector('.do-view-lead')->univ()->frameURL('Lead Details',[$this->api->url('xepan_marketing_leaddetails'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
+			$crud->grid->js('click')->_selector('.do-view-lead-visitor')->univ()->frameURL('Total Visits',[$this->api->url('xepan_marketing_leadvisitor'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
+			$crud->grid->js('click')->_selector('.do-view-lead-score')->univ()->frameURL('Total Score',[$this->api->url('xepan_marketing_leadscore'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
 		}
 	}
 }
---
+++ page/leadscore.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace xepan\marketing;
+	
+class page_leadscore extends \xepan\base\Page{
+	public $title = "Total Score";
+	function init(){
+		parent::init();
+
+		$lead_id=$this->app->stickyGET('contact_id');
+		
+		$score=$this->add('xepan\base\Model_PointSystem');
+		$score->addCondition('contact_id',$lead_id);
+		$score->tryLoadAny();
+		$grid = $this->add('xepan\base\Grid');
+		$grid->setModel($score);
+		$grid->addPaginator(50);
+	}
+}

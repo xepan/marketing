95083859a9d009a0195c354ed8e394d98fc56f52
sinharakesh <rksinha.btech@gmail.com>
2016-07-21 16:04:50 +0530

    working on chart graph

--- lib/View/BarChart.php
+++ lib/View/BarChart.php
@@ -1,22 +1,31 @@
 <?php
 
-
 namespace xepan\marketing;
 
 class View_BarChart extends \View{
 	function init(){
 		parent::init();
+
+		$chart = $this->add('xepan\base\View_Chart');
+		$chart->setChartType("Area");
+		$chart->setLibrary("Morris");
+		$chart->setXAxis('x');
+		$chart->setYAxis(['y', 'z']);
+		$data = [
+				['x'=> '2011 Q1', 'y'=> 3, 'z'=> 3],
+				['x'=> '2011 Q2', 'y'=> 2, 'z'=> 1],
+				['x'=> '2011 Q3', 'y'=> 2, 'z'=> 4],
+				['x'=> '2011 Q4', 'y'=> 3, 'z'=> 3]
+			];
+		$chart->setData($data);
+		$chart->setOption('behaveLikeLine',true);
+		$chart->setLabels(['Y', 'Z']);
 	}
 
 	function render(){
 		return parent::render();
 	}
-
-	function getJSID(){
-		return "graph-bar";
-	}
-
-	function defaultTemplate(){
-		return ['view/barchart'];
-	}
+	// function defaultTemplate(){
+	// 	return ['view/barchart'];
+	// }
 }
--- page/dashboard.php
+++ page/dashboard.php
@@ -7,40 +7,71 @@ class page_dashboard extends \xepan\base\Page{
 	function init(){
 		parent::init();
 		// HEADER FORM
-		$form = $this->add('Form',null,'form');
-		$form->setLayout('view\dashboard_form');
-		$form->addField('line','search','')->setAttr(['placeholder'=>'NLP Search']);
-		$cat_d = $form->addField('dropdown','category','');
-		$cat_d->setEmptyText('All');
-		$cat_d->setModel('xepan\marketing\Model_MarketingCategory');
-		$form->addField('line','daterange','')->setAttr(['placeholder'=>'Date range form field']);
+		// $form = $this->add('Form',null,'form');
+		// $form->setLayout('view\dashboard_form');
+		// $form->addField('line','search','')->setAttr(['placeholder'=>'NLP Search']);
+		// $cat_d = $form->addField('dropdown','category','');
+		// $cat_d->setEmptyText('All');
+		// $cat_d->setModel('xepan\marketing\Model_MarketingCategory');
+		// $form->addField('line','daterange','')->setAttr(['placeholder'=>'Date range form field']);
 
-		// HEADER FORM SUBMISSION
-		if($form->isSubmitted()){
-			throw new \Exception("SHOW RESULTS IN FRAME URL WITH GRAPHS");
-		}
+		// // HEADER FORM SUBMISSION
+		// if($form->isSubmitted()){
+		// 	throw new \Exception("SHOW RESULTS IN FRAME URL WITH GRAPHS");
+		// }
+
+		//GRAPH 1
+		// LEAD VS SCORE INCREMENT GRAPH
+		$from_date = "2001-01-01";
+		$to_date = $this->app->today;
+		$time_stamp = "Year";
+
+		$lead = $this->add('xepan\marketing\Model_Lead');
+		$lead->addCondition('created_at',">=",$from_date);
+		$lead->addCondition('created_at',"<=",$to_date);
+		$lead->addExpression('');
+
+		$tax_data = [
+			["Year"=> "2001", "Score"=> 20,'Lead'=>78],
+			["Year"=> "2001", "Score"=> 33, "Lead"=> 629],
+			["Year"=> "2003", "Score"=> 30, "Lead"=> 67],
+			["Year"=> "2004", "Score"=> 40, "Lead"=> 676],
+			["Year"=> "2005", "Score"=> 50, "Lead"=> 681],
+			["Year"=> "2006", "Score"=> 60, "Lead"=> 620],
+			["Year"=> "2007", "Score"=> 10, "Lead"=> 987],
+			["Year"=> "2008", "Score"=> 90, "Lead"=> 89]
+		];
+
+		$lead_vs_score = $this->add('xepan\base\View_Chart');
+		$lead_vs_score->setChartType("Line");
+		$lead_vs_score->setLibrary("Morris");
+		$lead_vs_score->setXAxis('Year');
+		$lead_vs_score->setYAxis(['Score','Lead']);
+		$lead_vs_score->setData($tax_data);
+		$lead_vs_score->setOption('behaveLikeLine',true);
+		$lead_vs_score->setLabels(['Score', 'Lead']);
 
 		// GRAPH AND CHART VIEWS
-		$bar_chart = $this->add('xepan\marketing\View_BarChart',null,'bar_chart');
-		$graph_stats = $this->add('xepan\marketing\View_GraphStats',null,'graph_stats');
+		$bar_chart = $this->add('xepan\marketing\View_BarChart');
+		// $graph_stats = $this->add('xepan\marketing\View_GraphStats',null,'graph_stats');
 		
 		// SOCIAL ACTIVITY LISTER
-		$social_lister = $this->add('xepan\marketing\View_SocialLister',null,'social_lister');
-		$social_lister->setModel('xepan\marketing\SocialPosters_Base_SocialConfig');
+		// $social_lister = $this->add('xepan\marketing\View_SocialLister',null,'social_lister');
+		// $social_lister->setModel('xepan\marketing\SocialPosters_Base_SocialConfig');
 
-		// MAP
-		$map = $this->add('xepan\marketing\View_Map',null,'map');
+		// // MAP
+		// $map = $this->add('xepan\marketing\View_Map',null,'map');
 
-		// LEAD RESPONSE
-		$lead_response = $this->add('xepan\marketing\View_LeadResponse',null,'lead_response',['view/leadresponse']);
+		// // LEAD RESPONSE
+		// $lead_response = $this->add('xepan\marketing\View_LeadResponse',null,'lead_response',['view/leadresponse']);
 		
-		// CAMPAIGN REPONSE
-		$campaign_response = $this->add('xepan\hr\Grid',null,'campaign_response',['view/campaignresponse']);
-		$campaign_response->setModel('xepan\marketing\Dashboard')->addCondition('ending_date','<',$this->app->today);
+		// // CAMPAIGN REPONSE
+		// $campaign_response = $this->add('xepan\hr\Grid',null,'campaign_response',['view/campaignresponse']);
+		// $campaign_response->setModel('xepan\marketing\Dashboard')->addCondition('ending_date','<',$this->app->today);
 	}
 
-	function defaultTemplate(){
-		return['page/dashboard'];
-	}
+	// function defaultTemplate(){
+	// 	return['page/dashboard'];
+	// }
 
 }

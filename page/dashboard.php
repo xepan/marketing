c53c9ffbcf5b63954d6ec2bb1976c03873b73a78
sinharakesh <rksinha.btech@gmail.com>
2016-07-21 22:26:18 +0530

    if null added

--- page/dashboard.php
+++ page/dashboard.php
@@ -99,17 +99,17 @@ class page_dashboard extends \xepan\base\Page{
 		
 		$lead->addExpression('last_landing_response_date_from_lead')->set(function($m,$q){
 			$landing_response = $m->add('xepan\marketing\Model_LandingResponse')->addCondition('contact_id',$m->getElement('id'))->setLimit(1)->setOrder('date','desc');
-			return $q->expr("COALESCE([0],'".$this->app->now."')",[$landing_response->fieldQuery('date')]);
+			return $q->expr("IFNULL([0],'".$this->app->now."')",[$landing_response->fieldQuery('date')]);
 		});
 
 		$lead->addExpression('last_communication_date_from_lead')->set(function($m,$q){
 			$communication = $m->add('xepan\communication\Model_Communication')->addCondition('from_id',$m->getElement('id'))->addCondition('direction','In')->setLimit(1)->setOrder('created_at','desc');
-			return $q->expr("COALESCE([0],'".$this->app->now."')",[$communication->fieldQuery('created_at')]);
+			return $q->expr("IFNULL([0],'".$this->app->now."')",[$communication->fieldQuery('created_at')]);
 		});
 
 		$lead->addExpression('last_communication_date_from_company')->set(function($m,$q){
 			$communication = $m->add('xepan\communication\Model_Communication')->addCondition('to_id',$m->getElement('id'))->addCondition('direction','Out')->setLimit(1)->setOrder('created_at','desc');
-			return $q->expr("COALESCE([0],'".$this->app->now."')",[$communication->fieldQuery('created_at')]);
+			return $q->expr("IFNULL([0],'".$this->app->now."')",[$communication->fieldQuery('created_at')]);
 		});
 
 		// current date - max from last_landing_from_lead, last_communication_form_lead or last_communication_form_employee

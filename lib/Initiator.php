f500ae6746b4dd3f43e06bca70a3879c2598fa9b
vaibhav <vaibhavsharma38@gmail.com>
2016-10-11 15:38:50 +0530

    small changes made

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -44,7 +44,7 @@ class Initiator extends \Controller_Addon {
 		$m->addItem(['Tele Marketing','icon'=>'fa fa-phone'],'xepan_marketing_telemarketing');
 		$m->addItem(['SMS','icon'=>'fa fa-envelope-square'],$this->app->url('xepan_marketing_sms',['status'=>'Draft,Submitted,Approved']));
 		$m->addItem(['Campaign','icon'=>'fa fa-bullhorn'],$this->app->url('xepan_marketing_campaign',['status'=>'Draft,Submitted,Redesign,Approved,Onhold']));
-		$m->addItem(['Reports','icon'=>'fa fa-cog'],'xepan_marketing_report');
+		// $m->addItem(['Reports','icon'=>'fa fa-cog'],'xepan_marketing_report');
 		$m->addItem(['Configuration','icon'=>'fa fa-cog'],'xepan_marketing_socialconfiguration');
 		
         $this->app->status_icon["xepan\marketing\Model_Lead"] = ['All'=>'fa fa-globe','Active'=>"fa fa-circle text-success",'InActive'=>'fa fa-circle text-danger'];

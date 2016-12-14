add5d82c9004a6a17d71acd2b38658e291ee1fe7
vaibhav <vaibhavsharma38@gmail.com>
2016-12-14 19:25:40 +0530

    category corrections

--- page/test.php
+++ page/test.php
@@ -5,43 +5,50 @@ namespace xepan\marketing;
 class page_test extends \xepan\base\Page{
 	function init(){
 		parent::init();
-		return;
-		$category_name = ['Default',
-					 'Active Affiliate',
-					 'InActive Affiliate',
-					 'Active Employee',
-					 'InActive Employee',
-					 'Active Customer',
-					 'InActive Customer',
-					 'Active Supplier',
-					 'InActive Supplier',
-					 'Active OutSourceParty',
-					 'InActive OutSourceParty'
-					];
-
-       	
-       	foreach ($category_name as $cat_name){
-        	$mar_cat=$this->add('xepan\marketing\Model_MarketingCategory');
-        	$mar_cat['name'] = $cat_name;
-        	$mar_cat['system'] = true;
-        	$mar_cat->save();			
-       	}
-
-       	$contact_type = ['Affiliate','Employee','Customer','Supplier','OutsourceParty'];
 		
-		foreach ($contact_type as $ct){
-			$model = $this->add('xepan\base\Model_Contact');
-			$model->addCondition('type',$ct);
+		$associate_and_keep_category = ['For ERP: Bhilwara textile','MACHINE MANUFACTURING IN INDIA','MANUFACTURING INDUSTRIES','Sales And Marketing Application','UCCI MEMBERS','UDAIPUR MACHINE MANUFACTURING INDUSTRIES','AHMEDABAD PRINTING INDUSTRIES','BANGALORE PRINTING INDUSTRIES'];
 		
-			foreach ($model as $m){			
+		$cat_name = ['PRINTING','PRODUCTION','ERP'];
+		foreach ($associate_and_keep_category as $keep_cat){
+			$mar_cat = $this->add('xepan\marketing\Model_MarketingCategory');			
+			$mar_cat->loadBy('name',$keep_cat);
+
+			$lead_cat_assoc = $this->add('xepan\marketing\Model_Lead_Category_Association'); 
+			$lead_cat_assoc->addCondition('marketing_category_id',$mar_cat->id);
+
+			foreach ($cat_name as $cn){
 				$mar_cat1 = $this->add('xepan\marketing\Model_MarketingCategory');
-				$cat_name = $m['status']." ".$ct;						
-	        	$mar_cat1->loadBy('name',$cat_name);
+				$mar_cat1->loadBy('name',$cn);					
+				
+				foreach ($lead_cat_assoc as $assoc){
+					$lead_cat_assoc1 = $this->add('xepan\marketing\Model_Lead_Category_Association'); 
+					$lead_cat_assoc1['lead_id'] = $assoc['lead_id'];
+					$lead_cat_assoc1['marketing_category_id'] = $mar_cat1->id;
+					$lead_cat_assoc1->save();	
+				}
+			}
+		}
 
-	        	$new_cat_assoc1 = $this->add('xepan\marketing\Model_Lead_Category_Association');
-				$new_cat_assoc1['lead_id'] = $m['id'];
-				$new_cat_assoc1['marketing_category_id'] = $mar_cat1->id;
-				$new_cat_assoc1->save();	
+
+		$associate_and_delete_category = ['AHMEDABAD PRINTING INDUSTRIES'];
+		$cat_name1 = ['Ahemedabaad'];
+		foreach ($associate_and_delete_category as $delete_cat){
+			$mar_cat2 = $this->add('xepan\marketing\Model_MarketingCategory');
+			$mar_cat2->loadBy('name',$delete_cat);
+
+			$lead_cat_assoc2 = $ths->add('xepan\marketing\Model_Lead_Category_Association'); 
+			$lead_cat_assoc2->addCondition('marketing_category_id',$mar_cat2->id);
+
+			foreach ($cat_name1 as $cn){
+				$mar_cat3 = $this->add('xepan\marketing\Model_MarketingCategory');
+				$mar_cat3->loadBy('name',$cn);					
+				
+				foreach ($lead_cat_assoc2 as $assoc){
+					$lead_cat_assoc3 = $this->add('xepan\marketing\Model_Lead_Category_Association'); 
+					$lead_cat_assoc3['lead_id'] = $assoc['lead_id'];
+					$lead_cat_assoc3['marketing_category_id'] = $mar_cat3->id;
+					$lead_cat_assoc3->save();	
+				}
 			}
 		}		
 	}

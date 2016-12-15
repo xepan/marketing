d3225952f1e01d1280be838e502d09d504be1aad
vaibhav <vaibhavsharma38@gmail.com>
2016-12-15 11:03:58 +0530

    deleting category association

--- page/test.php
+++ page/test.php
@@ -6,26 +6,46 @@ class page_test extends \xepan\base\Page{
 	function init(){
 		parent::init();
 		
-		$associate_and_delete_category = ['AHMEDABAD PRINTING INDUSTRIES'];
-		$cat_name1 = ['Ahemedabaad'];
-		foreach ($associate_and_delete_category as $delete_cat){
-			$mar_cat2 = $this->add('xepan\marketing\Model_MarketingCategory');
-			$mar_cat2->loadBy('name',$delete_cat);
+		// categories whose leads are wanted
+		$category_array = ['MANUFACTURING INDUSTRIES', 
+						  'Sales And Marketing Application',
+						  'UCCI MEMBERS', 
+						  'For ERP: Bhilwara textile',
+						  'UDAIPUR MACHINE MANUFACTURING INDUSTRIES',
+						  'MACHINE MANUFACTURING IN INDIA'];
+		
 
-			$lead_cat_assoc2 = $this->add('xepan\marketing\Model_Lead_Category_Association'); 
-			$lead_cat_assoc2->addCondition('marketing_category_id',$mar_cat2->id);
+		// gathering ids of categories 
+		$marketing_cateogry_id = []; 
+		foreach ($category_array as $cat){
+			$marketing_category = $this->add('xepan\marketing\Model_MarketingCategory');
+			$marketing_category->loadBy('name',$cat);
+			$marketing_cateogry_id [] = $marketing_category->id; 
+		}
 
-			foreach ($cat_name1 as $cn){
-				$mar_cat3 = $this->add('xepan\marketing\Model_MarketingCategory');
-				$mar_cat3->loadBy('name',$cn);					
-				
-				foreach ($lead_cat_assoc2 as $assoc){
-					$lead_cat_assoc3 = $this->add('xepan\marketing\Model_Lead_Category_Association'); 
-					$lead_cat_assoc3['lead_id'] = $assoc['lead_id'];
-					$lead_cat_assoc3['marketing_category_id'] = $mar_cat3->id;
-					$lead_cat_assoc3->save();	
-				}
-			}
-		}		
+		// gathering leads of cateogries from association
+		$lead_cat_assoc = $this->add('xepan\marketing\Model_Lead_Category_Association');
+		$lead_cat_assoc->addCondition('marketing_cateogry_id',$marketing_cateogry_id);
+
+		$lead_id = [];
+		foreach ($lead_cat_assoc as $assoc){
+			$lead_id [] = $assoc['lead_id'];
+		}
+
+		// getting id of printing category
+		$cat_name = 'PRINTING';
+		$category = $this->add('xepan\marketing\Model_MarketingCategory');
+		$category->loadBy('name',$cat_name);
+	
+		// deleting association with printing category
+		$lead_printing_assoc = $this->add('xepan\marketing\Model_Lead_Category_Association');
+		$lead_printing_assoc->addCondition('marketing_cateogry_id',$category->id);	
+		$lead_printing_assoc->addCondition('lead_id',$lead_id);
+
+		throw new \Exception($lead_printing_assoc->count());
+		
+		// foreach ($lead_cat_assoc as $assoc){
+		// 	$assoc->delete();
+		// }		
 	}
 }

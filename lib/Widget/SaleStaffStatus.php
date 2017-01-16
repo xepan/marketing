8755ac1b8024cfe817aedac7c22f57f5edeb93ee
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-01-16 18:40:15 +0530

    employee filter entity added in sales staff status

--- lib/Widget/SaleStaffStatus.php
+++ lib/Widget/SaleStaffStatus.php
@@ -8,6 +8,7 @@ class Widget_SaleStaffStatus extends \xepan\base\Widget {
 		parent::init();
 
 		$this->report->enableFilterEntity('date_range');
+		$this->report->enableFilterEntity('employee');
 
 		$this->chart = $this->add('xepan\base\View_Chart');
 	}
@@ -17,6 +18,10 @@ class Widget_SaleStaffStatus extends \xepan\base\Widget {
 		$this->end_date = $this->app->nextDate($this->report->end_date);
 		
 		$model = $this->add('xepan\hr\Model_Employee');
+	    
+		if(isset($this->report->employee))
+			$model->addCondition('id',$this->report->employee);
+
 	    $model->hasMany('xepan\marketing\Opportunity','assign_to_id',null,'Oppertunities');
 		$model->addExpression('Open')->set($model->refSQL('Oppertunities')->addCondition('status','Open')->addCondition('created_at','>',$this->start_date)->addCondition('created_at','<',$this->end_date)->sum('fund'));
 		$model->addExpression('Qualified')->set($model->refSQL('Oppertunities')->addCondition('status','Qualified')->addCondition('created_at','>',$this->start_date)->addCondition('created_at','<',$this->end_date)->sum('fund'));

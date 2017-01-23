9521da91e70d63968a911b17c53c0cd2d03a6689
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-01-23 18:48:47 +0530

    content icon added on content tab in schedule page

--- lib/View/ScheduleContent.php
+++ lib/View/ScheduleContent.php
@@ -4,8 +4,21 @@ namespace xepan\marketing;
 
 class View_ScheduleContent extends \CompleteLister{
 	function init(){
-		parent::init();
+		parent::init();		
+	}
 
+	function formatRow(){
+		parent::formatRow();
+
+		if($this->model['type'] == 'Newsletter'){
+			$this->current_row_html['icon'] = 'fa fa-globe';
+		}
+		elseif($this->model['type'] == 'SocialPost'){
+			$this->current_row_html['icon'] = 'fa fa-envelope';
+		}
+		else{
+			$this->current_row_html['icon'] = 'fa fa-mobile';
+		}	
 	}
 
 	function defaultTemplate(){

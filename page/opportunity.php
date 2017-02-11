8b328e11505a8b540dbdaaaa206ffab2d9a2eff3
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-02-11 17:05:06 +0530

    city and country added in filter added

--- page/opportunity.php
+++ page/opportunity.php
@@ -66,7 +66,7 @@ class page_opportunity extends \xepan\base\Page{
 		$crud->add('xepan\base\Controller_MultiDelete');		
 		$crud->add('xepan\base\Controller_Avatar',['name_field'=>'lead']);
 		
-		$f = $crud->grid->addQuickSearch(['lead','title','description',$x]);
+		$f = $crud->grid->addQuickSearch(['lead','title','description',$x,'city','country']);
 		$dropdown = $f->addField('dropdown','status')->setValueList(['Open'=>'Open','Converted'=>'Converted','Rejected'=>'Rejected'])->setEmptyText('Status');
 		$dropdown->js('change',$f->js()->submit());
 

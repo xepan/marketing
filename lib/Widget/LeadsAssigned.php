515ef3a20fd743ef9a32808b39dd6ccf3a913887
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-02-22 12:38:37 +0530

    lead assigned widget modified

--- lib/Widget/LeadsAssigned.php
+++ lib/Widget/LeadsAssigned.php
@@ -16,13 +16,23 @@ class Widget_LeadsAssigned extends \xepan\base\Widget {
 
 	function recursiveRender(){
 		$lead_m = $this->add('xepan\marketing\Model_Lead');
-		$lead_m->addCondition('created_at','>=',$this->report->start_date);
-		$lead_m->addCondition('created_at','<=',$this->app->nextDate($this->report->end_date));
-		
+		$lead_m->addCondition('assign_to_id','<>',null);
+
+		if($this->report->start_date)
+			$lead_m->addCondition('created_at','>=',$this->report->start_date);
+		else
+			$lead_m->addCondition('created_at','>=',$this->app->today);
+
+		if($this->report->end_date)
+			$lead_m->addCondition('created_at','<=',$this->app->nextDate($this->report->end_date));
+		else
+			$lead_m->addCondition('created_at','<=',$this->app->nextDate($this->app->today));
+
 		if(isset($this->report->employee))
 			$lead_m->addCondition('assign_to_id',$this->report->employee);
 
 		$this->grid->setModel($lead_m,['name','created_by','assign_to','source']);
+		$this->grid->addPaginator(20);
 
 		return parent::recursiveRender();
 	}

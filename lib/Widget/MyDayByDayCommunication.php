f0bafe5f78e0d683b1de605cd51d7060f7d8fbb2
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-02-22 10:37:50 +0530

    date range condition changed

--- lib/Widget/MyDayByDayCommunication.php
+++ lib/Widget/MyDayByDayCommunication.php
@@ -11,16 +11,19 @@ class Widget_MyDayByDayCommunication extends \xepan\base\Widget {
 	}
 
 	function recursiveRender(){
-		
 		$communication_graph = $this->add('xepan\communication\Model_Communication');
 		$communication_graph->addExpression('date','date(created_at)');
 		$communication_graph->addExpression('score','count(*)');
-		if(isset($this->report->employee))
-			$communication_graph->addCondition([['from_id',$this->app->employee->id],['to_id',$this->app->employee->id]]);
+		
 		if(isset($this->report->start_date))
-			$communication_graph->addCondition('created_at','>',$this->report->start_date);
+			$communication_graph->addCondition('created_at','>=',$this->report->start_date);
+		else
+			$communication_graph->addCondition('created_at','>=',$this->app->today);
+		
 		if(isset($this->report->end_date))
-			$communication_graph->addCondition('created_at','<',$this->app->nextDate($this->report->end_date));
+			$communication_graph->addCondition('created_at','<=',$this->app->nextDate($this->report->end_date));
+		else
+			$communication_graph->addCondition('created_at','<=',$this->app->nextDate($this->app->today));
 		
 		$communication_graph->addCondition('status','<>','Outbox');
 		$communication_graph->setOrder('date','asc')
@@ -33,9 +36,7 @@ class Widget_MyDayByDayCommunication extends \xepan\base\Widget {
 		}
 
 		$data_array = array_values($data_array);
-		// echo "<pre>";
-		// var_dump($data_array);
-		// exit;
+		
 		$this->chart = $this->add('xepan\base\View_Chart')
 	 		->setType('bar')
 	 		->setData(['json'=>$data_array])
@@ -43,8 +44,7 @@ class Widget_MyDayByDayCommunication extends \xepan\base\Widget {
 	 		->setXAxis('date')
 	 		->setYAxises(['Email','Newsletter','Call','Personal','Comment','TeleMarketing'])
 	 		->addClass('col-md-12')
-	 		->setTitle('Communication')
-	 		;
+	 		->setTitle('Communication');
 
 		return parent::recursiveRender();
 	}

104be3118ae0065ed00c176b815f7522e730c7fb
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-03-24 11:54:09 +0530

    BUG => in_array null value passed in second parameter solved

--- page/daybydayanalytics.php
+++ page/daybydayanalytics.php
@@ -14,8 +14,9 @@ class page_daybydayanalytics extends \xepan\base\Page{
         $rpt = $this->add('xepan\base\Model_GraphicalReport');
         $rpt->tryLoadBy('name','MarketingReport'); 
            
-        $permitted_post = json_decode($rpt['permitted_post'],true);      
-        if((in_array($this->app->employee['post_id'], $permitted_post)) OR $this->app->employee['scope'] == 'SuperUser')    
+        $permitted_post = json_decode($rpt['permitted_post'],true);
+
+        if((count($permitted_post) AND in_array($this->app->employee['post_id'], $permitted_post)) OR $this->app->employee['scope'] == 'SuperUser')    
             $runner_view = $this->add('xepan\hr\View_GraphicalReport_Runner',['report_id'=>$rpt->id]);   
     }
 

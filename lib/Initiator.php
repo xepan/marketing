d4f4ac1f0c41cd9154c5dc9bb584b72ecc8d4e57
vijay mali <guddavijay@gmail.com>
2017-08-10 17:00:05 +0530

    employee lead related report created

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -55,6 +55,7 @@ class Initiator extends \Controller_Addon {
 			$m->addItem(['Configuration','icon'=>'fa fa-cog'],'xepan_marketing_socialconfiguration');
 
 			$this->app->user_menu->addItem(['My Lead','icon'=>'fa fa-file-text-o'],$this->app->url('xepan_marketing_mylead'));
+			$this->app->report_menu->addItem(['Employee Lead Report','icon'=>'fa fa-users'],$this->app->url('xepan_marketing_report_employeeleadreport'));
 			
 		}
 		

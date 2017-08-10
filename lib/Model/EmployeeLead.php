a8134e2b0cf6e81fd1660e43b366dac89b8981b2
vijay mali <guddavijay@gmail.com>
2017-08-10 17:00:13 +0530

    employee lead related report created

---
+++ lib/Model/EmployeeLead.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace xepan\marketing;
+
+/**
+* 
+*/
+class Model_EmployeeLead extends \xepan\hr\Model_Employee{
+	public $from_date;
+	public $to_date;
+	function init(){
+		parent::init();
+		$this->addCondition('status','Active');
+
+		$this->addExpression('total_lead_created')->set(function($m,$q){
+			// return $q->getField('id');
+			$lead = $this->add('xepan\marketing\Model_Lead',['table_alias'=>'employee_created_lead']);
+				return 	$lead->addCondition('created_by_id',$q->getField('id'))
+						// ->addCondition('created_at','>=',$this->from_date)
+						// ->addCondition('created_at','<',$this->api->nextDate($this->to_date))
+						->count();
+		});
+
+		$this->addExpression('total_lead_assign_to')->set(function($m,$q){
+			// return $q->getField('id');
+			$lead = $this->add('xepan\marketing\Model_Lead',['table_alias'=>'employee_assign_lead_to']);
+				return 	$lead->addCondition('created_by_id',$q->getField('id'))
+						// ->addCondition('created_at','>=',$this->from_date)
+						// ->addCondition('created_at','<',$this->api->nextDate($this->to_date))
+						->count();
+		});
+
+		$this->addExpression('total_lead_assign_by')->set(function($m,$q){
+			// return $q->getField('id');
+			$lead = $this->add('xepan\marketing\Model_Lead',['table_alias'=>'employee_assign_lead_by']);
+				return 	$lead->addCondition('assign_to_id',$q->getField('id'))
+						// ->addCondition('created_at','>=',$this->from_date)
+						// ->addCondition('created_at','<',$this->api->nextDate($this->to_date))
+						->count();
+		});
+
+		$this->addExpression('total_followup')->set(function($m,$q){
+			$my_followups_model = $this->add('xepan\projects\Model_Task');
+		    return $my_followups_model->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition('created_by_id',$q->getField('id'))
+		    						->addCondition('type','Followup')
+		    						->count();
+		});
+
+		$this->addExpression('open_opportunity')->set(function($m,$q){
+			$opportunity = $this->add('xepan\marketing\Model_Opportunity');
+		    return $opportunity->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition([['created_by_id',$q->getField('id')],['assign_to_id',null]])
+		    						->addCondition('status','Open')
+		    						->count();
+		});
+
+		$this->addExpression('qualified_opportunity')->set(function($m,$q){
+			$opportunity = $this->add('xepan\marketing\Model_Opportunity');
+		    return $opportunity->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition([['created_by_id',$q->getField('id')],['assign_to_id',null]])
+		    						->addCondition('status','Qualified')
+		    						->count();
+		});
+
+		$this->addExpression('needs_analysis_opportunity')->set(function($m,$q){
+			$opportunity = $this->add('xepan\marketing\Model_Opportunity');
+		    return $opportunity->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition([['created_by_id',$q->getField('id')],['assign_to_id',null]])
+		    						->addCondition('status','NeedsAnalysis')
+		    						->count();
+		});
+
+		$this->addExpression('quoted_opportunity')->set(function($m,$q){
+			$opportunity = $this->add('xepan\marketing\Model_Opportunity');
+		    return $opportunity->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition([['created_by_id',$q->getField('id')],['assign_to_id',null]])
+		    						->addCondition('status','Quoted')
+		    						->count();
+		});
+
+		$this->addExpression('negotiated_opportunity')->set(function($m,$q){
+			$opportunity = $this->add('xepan\marketing\Model_Opportunity');
+		    return $opportunity->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition([['created_by_id',$q->getField('id')],['assign_to_id',null]])
+		    						->addCondition('status','Negotiated')
+		    						->count();
+		});
+
+		$this->addExpression('win_opportunity')->set(function($m,$q){
+			$opportunity = $this->add('xepan\marketing\Model_Opportunity');
+		    return $opportunity->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition([['created_by_id',$q->getField('id')],['assign_to_id',null]])
+		    						->addCondition('status','Won')
+		    						->count();
+		});
+
+		$this->addExpression('loss_opportunity')->set(function($m,$q){
+			$opportunity = $this->add('xepan\marketing\Model_Opportunity');
+		    return $opportunity->addCondition('assign_to_id',$q->getField('id'))
+		    						->addCondition([['created_by_id',$q->getField('id')],['assign_to_id',null]])
+		    						->addCondition('status','Lost')
+		    						->count();
+		});
+	}
+}
---
+++ page/report/employeeleadreport.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace xepan\marketing;
+
+/**
+* 
+*/
+class page_report_employeeleadreport extends \xepan\base\Page{
+
+	public $title = "Employee Lead Report`s";
+	function init(){
+		parent::init();
+		$emp_id = $this->app->stickyGET('employee_id');
+		// $from_date = $this->app->stickyGET('from_date');
+		// $to_date = $this->app->stickyGET('to_date');
+		$form = $this->add('Form',null,null,['form/empty']);
+		// $date = $form->addField('DateRangePicker','date_range');
+		// $set_date = $this->app->today." to ".$this->app->today;
+		// if($from_date){
+		// 	$set_date = $from_date." to ".$to_date;
+		// 	$date->set($set_date);	
+		// }
+		$emp_field = $form->addField('xepan\base\Basic','employee');
+		$emp_field->setModel('xepan\hr\Model_Employee')->addCondition('status','Active');
+		
+		$emp_model = $this->add('xepan\marketing\Model_EmployeeLead'/*,['from_date'=>$from_date,'to_date'=>$to_date]*/);
+		if($emp_id){
+			$emp_model->addCondition('id',$emp_id);
+		}
+		// if($_GET['from_date']){
+		// 	$emp_model->from_date = $_GET['from_date'];
+		// }
+		$form->addSubmit('Get Details')->addClass('btn btn-primary');
+
+		$grid = $this->add('xepan\hr\Grid',null,null,['view/report/employee-lead-report-gridview']);
+		$grid->setModel($emp_model);
+		$grid->addPaginator(50);
+
+		if($form->isSubmitted()){
+
+			$grid->js()->reload(['employee_id'=>$form['employee']])->execute();
+			
+			// $form->js()->univ()->redirect($this->app->url(),[
+			// 					'employee_id'=>$form['employee'],
+			// 					'from_date'=>$date->getStartDate()?:0,
+			// 					'to_date'=>$date->getEndDate()?:0
+			// 				]
+							
+			// 			)->execute();
+		}
+	}
+}

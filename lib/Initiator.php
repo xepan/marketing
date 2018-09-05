65c6f9798f9ec2fe819f1f69d6912efdf97fadb6
sinharakesh <rksinha.btech@gmail.com>
2018-09-05 09:24:45 +0530

    report menu exported

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -87,7 +87,7 @@ class Initiator extends \Controller_Addon {
 			// $m->addItem(['Configuration','icon'=>'fa fa-cog'],'xepan_marketing_socialconfiguration');
 
 			$this->app->user_menu->addItem(['My Sales','icon'=>'fa fa-file-text-o'],$this->app->url('xepan_marketing_mylead'));
-			$this->app->report_menu->addItem(['Employee Lead Report','icon'=>'fa fa-users'],$this->app->url('xepan_marketing_report_employeeleadreport'));
+			// $this->app->report_menu->addItem(['Employee Lead Report','icon'=>'fa fa-users'],$this->app->url('xepan_marketing_report_employeeleadreport'));
 			
 		}
 	}
@@ -171,6 +171,12 @@ class Initiator extends \Controller_Addon {
 							'url'=>'xepan_marketing_report_employeeleadreport',
 							'skip_default'=>true
 						]
+				],
+				'Reports'=>[
+					[	'name'=>'Employee Lead Report',
+						'icon'=>'fa fa-users',
+						'url'=>'xepan_marketing_report_employeeleadreport'
+					]
 				]
 			];
 			// $m->addItem(['Reports','icon'=>'fa fa-cog'],'xepan_marketing_report');
